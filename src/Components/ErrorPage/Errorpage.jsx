33ec9f7a94c77fc11ce3dd601d14e08e5c322497
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2024-12-10 10:39:32 +0600

    added a responsive navbar

---
+++ src/Components/ErrorPage/Errorpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Errorpage = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">page not found</h2>
+            <p>Status: 404</p>
+        </div>
+    );
+};
+
+export default Errorpage;
