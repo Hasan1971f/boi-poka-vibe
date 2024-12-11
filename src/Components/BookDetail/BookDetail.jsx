afb96fb433775dbff3f912fd446ee74dfda4f47b
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2024-12-11 08:41:52 +0600

    added a save mark as read to some where

--- src/Components/BookDetail/BookDetail.jsx
+++ src/Components/BookDetail/BookDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { addToStoredReadList } from '../Utitlity/addtoDb';
 
 const BookDetail = () => {
     const {bookId} =useParams()
@@ -11,6 +12,19 @@ const BookDetail = () => {
    
     const {bookId: currentBookId, image} = book
 
+    const handleMarkAsRead = (id)=>{
+    
+    //   1. Understand what to store or save : => bookId
+    //   2. where to store : database 
+    //   3. array , list, collection
+    //   4. check: if the book is already in the read list.
+    //   5. if not, theb add the book  to the list 
+    //   6. if yes, do not add the book
+
+    addToStoredReadList (id)
+
+    }
+
   
 
    
@@ -21,8 +35,8 @@ const BookDetail = () => {
             <h2>Book details : {bookId}</h2>
             <img className='w-36' src={image} alt="" />
             <br/>
-            <button className="btn btn-outline mr-4 btn-accent">Read</button>
-            <button className="btn btn-accent">Whist List</button>
+            <button onClick={ ()=> handleMarkAsRead (bookId)} className="btn btn-outline mr-4 btn-accent"> Mark as Read</button>
+            <button className="btn btn-accent"> Add to Whist List</button>
         </div>
     );
 };
